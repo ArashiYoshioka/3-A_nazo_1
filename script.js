32043ec6d5d292c3077e31e3547236e4bd7a77a7
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-06 10:15:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const result = document.getElementById('result');
 
     if (answerInput === "28") {
-        result.textContent = "出席番号3番";
+        result.textContent = "正解！ 出席番号3番";
     } else {
         result.textContent = "残念...";
     }
